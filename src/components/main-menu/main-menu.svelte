73f7de9a94444f8499801359304631108e2b0591
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:55:56 +0700

    Add main-menu component

---
+++ src/components/main-menu/main-menu.svelte
@@ -0,0 +1,18 @@
+<nav class="main-menu">
+  <div class="main-menu__item">
+    <a href="/alarms">Установить будильник</a>
+  </div>
+  <div class="main-menu__item">
+    <a href="/i-am-alarm">Стать будильником</a>
+  </div>
+</nav>
+
+<style>
+  .main-menu {
+    display: flex;
+  }
+
+  .main-menu__item + .main-menu__item {
+    margin-left: 20px;
+  }
+</style>
