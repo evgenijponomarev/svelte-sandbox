27f106640ea597e1de82252f9db41653cefa0724
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:56:36 +0700

    Build app layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  
+  import Layout from './components/layout/layout.svelte';
+  import Header from './components/header/header.svelte';
+  import Footer from './components/footer/footer.svelte';
 </script>
 
-<main>
-  
-</main>
+<Layout>
+  <Header/>
 
-<style>
-  
-</style>
+  <Footer/>
+</Layout>
