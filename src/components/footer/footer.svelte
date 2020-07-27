a6bc9a35e2a4f686841911f6d2518ab0e39465a6
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:55:19 +0700

    Add footer component

---
+++ src/components/footer/footer.svelte
@@ -0,0 +1,24 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<footer class="footer">
+  <div class="footer__copyright">Будильник на Svelte {year}</div>
+
+  <div class="footer__about-link">
+    <a href="/about">О сервисе</a>
+  </div>
+</footer>
+
+<style>
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    border-top: 1px solid #ddd;
+  }
+
+  .footer__copyright {
+    color: #999;
+  }
+</style>
