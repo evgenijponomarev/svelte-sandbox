0de03c0858fc0a25ebef40a1b0a442ec7b3c7be4
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-28 00:56:11 +0700

    Add button component

---
+++ src/components/button/button.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let to;
+</script>
+
+{#if to}
+  <a href={to} class="button">
+    <slot/>
+  </a>
+{:else}
+  <button class="button">
+    <slot/>
+  </button>
+{/if}
+
+<style>
+  .button {
+    display: inline-block;
+    border: 2px solid #ddd;
+    background: none;
+    padding: 8px 20px;
+    border-radius: 4px;
+    color: #444;
+  }
+
+  .button:hover {
+    text-decoration: none;
+    background-color: #ddd;
+  }
+</style>
