8dfb0628565d8534c34d6681343e3af9b6864500
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-28 00:55:05 +0700

    Add routes into app layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,27 @@
 <script>
+  import Router from 'svelte-spa-router'
+
   import Layout from './components/layout/layout.svelte';
   import Header from './components/header/header.svelte';
   import Footer from './components/footer/footer.svelte';
+  import ContentPlace from './components/content-place/content-place.svelte';
+
+  import Alarms from './views/alarms/alarms.svelte';
+  import About from './views/about/about.svelte';
+
+  const routes = {
+    '/': Alarms,
+    '/alarms': Alarms,
+    '/about': About,
+  };
 </script>
 
 <Layout>
   <Header/>
+
+  <ContentPlace>
+    <Router {routes}/>
+  </ContentPlace>
 
   <Footer/>
 </Layout>
