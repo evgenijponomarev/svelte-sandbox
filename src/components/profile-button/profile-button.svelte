4d15c1ab6627c8bce7ce5d0476b8fc36ca9f1e28
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:56:07 +0700

    Add profile-button component

---
+++ src/components/profile-button/profile-button.svelte
@@ -0,0 +1,13 @@
+<a href="/profile" class="profile">
+  Профиль
+</a>
+
+<style>
+  .profile {
+    padding-left: 28px;
+    background-image: url('./profile-button.png');
+    background-position: left center;
+    background-size: 22px;
+    background-repeat: no-repeat;
+  }
+</style>
