2c29c11d54a10b6ad4c99f034457af5b5c74b06d
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:55:04 +0700

    Add header component

---
+++ src/components/header/header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import MainMenu from '../main-menu/main-menu.svelte';
+  import ProfileButton from '../profile-button/profile-button.svelte';
+</script>
+
+<header class="header">
+  <div class="header__title">Будильник</div>
+
+  <MainMenu/>
+
+  <ProfileButton/>
+</header>
+
+<style>
+  .header {
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  .header__title {
+    font-size: 24px;
+  }
+</style>
