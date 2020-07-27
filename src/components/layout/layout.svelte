b1074bc2e01f298665779651db7965e5e9745163
evgeniy1801 <evgeniy1801@gmail.com>
2020-07-27 23:55:38 +0700

    Add layout component

---
+++ src/components/layout/layout.svelte
@@ -0,0 +1,14 @@
+<div class="layout">
+  <slot/>
+</div>
+
+<style>
+  .layout {
+    max-width: 800px;
+    margin: 0 auto;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
