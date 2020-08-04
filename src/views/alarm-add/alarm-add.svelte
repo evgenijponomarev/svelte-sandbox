af5253a90374599a7e88dd8e1f6a1c82dc6982d4
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-04 17:04:49 +0700

    Add dialog form pages

---
+++ src/views/alarm-add/alarm-add.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { pop } from 'svelte-spa-router';
+
+  import Alarms from '../alarms/alarms.svelte';
+
+  import DialogOverlay from '../../components/dialog-overlay/dialog-overlay.svelte';
+  import DialogAlarmForm from '../../components/dialog-alarm-form/dialog-alarm-form.svelte';
+
+  function onCloseDialog() {
+    pop();
+  }
+</script>
+
+<Alarms/>
+
+<DialogAlarmForm onCLose={onCloseDialog}/>
---
+++ src/views/alarm-edit/alarm-edit.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { pop } from 'svelte-spa-router';
+
+  import Alarms from '../alarms/alarms.svelte';
+
+  import DialogOverlay from '../../components/dialog-overlay/dialog-overlay.svelte';
+  import DialogAlarmForm from '../../components/dialog-alarm-form/dialog-alarm-form.svelte';
+
+  export let params = {};
+
+  function onCloseDialog() {
+    pop();
+  }
+
+  const alarm = {
+    id: params.id,
+    time: '10:00',
+    repeat: 'workdays',
+  };
+</script>
+
+<Alarms/>
+
+<DialogAlarmForm onCLose={onCloseDialog} {alarm}/>
