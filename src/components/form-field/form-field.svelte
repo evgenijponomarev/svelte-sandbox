88982eca8115f4d3329c0d9e88689e2d86336c81
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-04 17:03:05 +0700

    Add FormField component

---
+++ src/components/form-field/form-field.svelte
@@ -0,0 +1,44 @@
+<script>
+  import TextField from '../text-field/text-field';
+  import SelectField from '../select-field/select-field';
+
+  export let id = '';
+  export let name = '';
+  export let label = '';
+  export let type;
+  export let options;
+  export let value;
+
+  const params = { id, name };
+</script>
+
+<div class="form-field">
+  <label for={id} class="form-field__label">
+    {label}
+  </label>
+
+  {#if type === 'select'}
+    <SelectField
+      mix="form-field__control"
+      {options}
+      bind:value={value}
+      {...params}
+    />
+  {:else}
+    <TextField
+      mix="form-field__control"
+      {type}
+      bind:value={value}
+      {...params}
+    />
+  {/if}
+</div>
+
+<style>
+  .form-field__label {
+    display: block;
+    color: #333;
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+</style>
