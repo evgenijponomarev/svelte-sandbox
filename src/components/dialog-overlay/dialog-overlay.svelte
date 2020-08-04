05f4201665755ed5a6e4c6067e23fcb13c39a7aa
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-04 17:02:37 +0700

    Add DialogOverlay component

---
+++ src/components/dialog-overlay/dialog-overlay.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let onClose = () => {};
+
+  function onKeyDown(event) {
+    event.key === 'Escape' && onClose();
+  }
+</script>
+
+<svelte:body on:keydown={onKeyDown}/>
+
+<div class="dialog-overlay" on:click|self={onClose}>
+  <slot/>
+</div>
+
+<style>
+  .dialog-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0,0,0,.5);
+  }
+</style>
