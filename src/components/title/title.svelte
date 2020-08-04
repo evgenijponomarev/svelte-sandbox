bf565c4c0fe57ea2126b12443b037476696443f7
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-04 17:03:43 +0700

    Add Title component

---
+++ src/components/title/title.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let mix = '';
+</script>
+
+<h1 class={['title', mix].join(' ')}>
+  <slot/>
+</h1>
+
+<style>
+  .title {
+    font-size: 32px;
+    color: #333;
+    font-weight: lighter;
+    margin: 0;
+    padding: 0 0 20px;
+  }
+</style>
