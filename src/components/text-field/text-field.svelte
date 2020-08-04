e9282c52d4ff9f83ac7d907050cb7380fe43f5f2
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-04 17:03:30 +0700

    Add TextField component

---
+++ src/components/text-field/text-field.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let type = 'text';
+  export let mix = '';
+  export let id = '';
+  export let name = '';
+  export let value = '';
+
+  const params = {
+    type,
+    id,
+    name,
+    'bind:value': value,
+  };
+</script>
+
+<input
+  class={['text-field', mix].join(' ')}
+  bind:value={value}
+  {...params}
+/>
+
+<style>
+  .text-field {
+    width: 100%;
+    height: 40px;
+    box-sizing: border-box;
+    line-height: 40px;
+    padding: 0 5px;
+    border: 1px solid #777;
+    border-radius: 0;
+    outline: none;
+  }
+
+  .text-field:focus {
+    box-shadow: inset 0 0 10px -5px rgba(0,0,0,.5);
+  }
+</style>
