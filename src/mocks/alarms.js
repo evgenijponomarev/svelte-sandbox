412514a57b6b7fc39ebda5c62347cebbfb8d5601
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-05 20:11:38 +0700

    Add alarm list mocks

---
+++ src/mocks/alarms.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    time: '8:00',
+    repeat: 'workdays',
+  },
+  {
+    id: 2,
+    time: '8:05',
+    repeat: 'workdays',
+  },
+  {
+    id: 3,
+    time: '8:10',
+    repeat: 'workdays',
+  },
+  {
+    id: 4,
+    time: '8:10',
+    repeat: 'workdays',
+  },
+  {
+    id: 5,
+    time: '8:15',
+    repeat: 'workdays',
+  },
+  {
+    id: 6,
+    time: '10:00',
+    repeat: 'weekends',
+  },
+];
