81a03861ea9a59d587b0d2c237c06366bb62b715
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-05 20:09:48 +0700

    Add AlarmList component

---
+++ src/components/alarm-list/alarm-list.svelte
@@ -0,0 +1,35 @@
+<script>
+  import AlarmCard from '../../components/alarm-card/alarm-card';
+
+  export let alarms = [];
+</script>
+
+<div class="alarm-list">
+  {#each alarms as alarm}
+    <div class="alarm-list__item">
+      <AlarmCard
+        link="#/alarms/{alarm.id}"
+        time={alarm.time}
+        repeat={alarm.repeat}
+      />
+    </div>
+  {/each}
+</div>
+
+<style>
+  .alarm-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -10px;
+    padding-top: 10px;
+  }
+
+  .alarm-list__item {
+    width: 50%;
+    max-width: 400px;
+    min-width: 200px;
+    box-sizing: border-box;
+    flex-grow: 1;
+    padding: 10px;
+  }
+</style>
