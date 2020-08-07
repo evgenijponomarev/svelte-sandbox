a244833a769c0baaaa148881821f846d65b3e289
evgeniy1801 <evgeniy1801@gmail.com>
2020-08-07 17:31:01 +0700

    Improvement mockProvider

--- src/mocks.js
+++ src/mocks.js
@@ -5,7 +5,28 @@ import ALARMS from './mocks/alarms';
 
 const mock = new MockAdapter(axios, { delayResponse: 1000 });
 
-mock.onGet('/alarms').reply(200, {
-  status: 'ok',
-  items: ALARMS,
-});
+function log(request, response) {
+  console.log(`Request ${request.method.toUpperCase()} ${request.url}`);
+  if (request.data) {
+    console.log('with body:');
+    console.log(request.data);
+  }
+  console.log('Response:');
+  console.log(response);
+}
+
+function reply(data) {
+  return function(config) {
+    log(config, data);
+    
+    return [200, data];
+  }
+}
+
+function replyOk(data) {
+  return reply({ status: 'ok', ...data });
+}
+
+mock.onGet('/alarms').reply(replyOk({ items: ALARMS }));
+
+mock.onPost('/alarms').reply(replyOk());
